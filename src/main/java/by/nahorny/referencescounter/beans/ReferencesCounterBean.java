eadf5989520470a02555c1c8b3ec6dd4512b1060
Vadim Nahorny <spektor777@gmail.com>
2022-11-18 11:50:32 +0200

    fix some bug and add feature

--- src/main/java/by/nahorny/referencescounter/beans/ReferencesCounterBean.java
+++ src/main/java/by/nahorny/referencescounter/beans/ReferencesCounterBean.java
@@ -2,16 +2,23 @@ package by.nahorny.referencescounter.beans;
 
 import by.nahorny.referencescounter.entity.Reference;
 import by.nahorny.referencescounter.service.ReferenceService;
+import by.nahorny.referencescounter.service.Strings.Message;
 import by.nahorny.referencescounter.service.UrlValidatorSingleton;
 import lombok.Data;
 import org.apache.commons.validator.routines.UrlValidator;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.List;
+
+import static by.nahorny.referencescounter.service.Strings.Message.*;
 
 @ManagedBean
 @ViewScoped
@@ -19,38 +26,37 @@ import java.util.List;
 public class ReferencesCounterBean {
 
     String path;
-    String path1;
-    List<Reference> referencesList;
+    List<Reference> referencesList = new ArrayList<>();
+
 
     public List<Reference> workWithReference() throws MalformedURLException, IOException {
-
-        System.out.println(path);
-        UrlValidator urlValidator = UrlValidatorSingleton.getUrlValidator();
-        if (urlValidator.isValid(path)) {
-            referencesList = new ReferenceService().getAllReferences(new Reference(path));
-        } else {
-            System.out.println("Введенная строка не является адресом");
-            path = "";
+        try {
+            System.out.println(path);
+            UrlValidator urlValidator = UrlValidatorSingleton.getUrlValidator();
+            if (urlValidator.isValid(path)) {
+                referencesList = new ReferenceService().getAllReferences(new Reference(path));
+            } else {
+                referencesList.clear();
+                FacesContext.getCurrentInstance().addMessage(null,
+                        new FacesMessage(STRING_NOT_URL, STRING_NOT_URL));
+            }
+        } catch (UnknownHostException ignored) {
+            referencesList.clear();
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(UNKNOWHOSTEXCEPTION, UNKNOWHOSTEXCEPTION));
         }
-        System.out.println("лист ссылок: " + referencesList);
-        System.out.println("Метод workWithReference закончил выполняться");
         return referencesList;
     }
 
     public void getOneReference() {
-        System.out.println("метод getOneReference начал выполняться");
         path = FacesContext.getCurrentInstance().
                 getExternalContext().getRequestParameterMap().get("oneReferenceHidden");
-        System.out.println(path);
-        System.out.println("метод getOneReference закончил выполняться");
 
     }
 
-    public String clearTable() {
-        System.out.println("метод clearTable выполняется");
-        if (referencesList!=null)
-        referencesList.clear();
+    public  void clearTable() {
+        if (referencesList!= null)
+            referencesList.clear();
         path = null;
-        return "referenceCounter.xhtml";
     }
 }
---
+++ src/main/java/by/nahorny/referencescounter/service/Strings/Message.java
@@ -0,0 +1,7 @@
+package by.nahorny.referencescounter.service.Strings;
+
+public class Message {
+    public static final String STRING_NOT_URL = "Введенная строка не является\nURL-адресом.";
+    public static final String UNKNOWHOSTEXCEPTION = "Ошибка соединения по введенному URL.";
+
+}
